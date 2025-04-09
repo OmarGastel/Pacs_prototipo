2a4794bff7b3d059157b698254f2f9be0ad7a3e0
Omar Gil <omar.gil.gastel@gmail.com>
2025-04-09 11:54:59 -0600

    eaaa
    aa

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php include(__DIR__ . '/../includes/header.php'); ?>
+<?php include __DIR__ . '/../includes/header.php'; ?>
 
 <head>
   ...
@@ -443,4 +443,4 @@
 
 
 
-<?php include(__DIR__ . '/../includes/footer.php'); ?>
+<?php include __DIR__ . '/../includes/footer.php'; ?>
